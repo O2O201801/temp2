edb601d71a9ef7c623862876c84ca67738d9a34a
SuYunPark <spark5@nate.com>
2018-06-01 02:19:54 +0900

    register complete
    complete

--- SimpleAPI/app.js
+++ SimpleAPI/app.js
@@ -4,25 +4,25 @@ var app = express();
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
-              host     : '172.16.9.223',
+              host     : '192.168.35.49',
               user     : 'root',
               password : '0523',
               database : 'o2o'
             });
- 
+
 connection.connect();
 connection.on('error', function(err) {
   console.log("[mysql error]",err);
 });
- 
+
 global.db = connection;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var routes = require("./routes/routes.js")(app);
+var routes = require("./routes/register.js")(app);
 
-var server = app.listen(3000,'172.16.9.223', function () {
+var server = app.listen(6010,'192.168.35.49', function () {
     console.log("Listening on port %s...", server.address().port);
 
 
